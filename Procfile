685dcd173bb74e1f1830b8fa895022a48364ce11
IgorPereira1997 <pereira.t.igor@gmail.com>
2021-05-24 23:14:36 -0300

    App Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ProjetoSD_2.wsgi
--- ProjetoSD_2/settings.py
+++ ProjetoSD_2/settings.py
@@ -152,3 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
