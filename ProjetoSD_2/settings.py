6203a45f961ca3d486acd0d74abefe80a0523f18
IgorPereira1997 <pereira.t.igor@gmail.com>
2021-06-04 10:32:50 -0300

    Added custom errors pages

--- ProjetoSD_2/settings.py
+++ ProjetoSD_2/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contato',
+    'errors',
     'listar_produtos',
     'listar_transportadoras',
     'login',
